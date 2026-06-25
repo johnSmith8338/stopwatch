589d572b5f08e7843686e8ff6aded606db04ba24
Dmitriy <john.smith.dm8338@gmail.com>
2026-06-25 14:46:44 +0300

    - add 3d-effect to wheel picker

--- src/app/components/wheel-picker/wheel-picker.ts
+++ src/app/components/wheel-picker/wheel-picker.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, ChangeDetectionStrategy, Component, effect, ElementRef, input, output, viewChild } from '@angular/core';
+import { afterNextRender, ChangeDetectionStrategy, Component, computed, effect, ElementRef, input, output, signal, viewChild } from '@angular/core';
 import { WHEEL_HEIGHT, WHEEL_ITEM_HEIGHT, WHEEL_OFFSET } from '../../constants/wheel-picker.constants';
 
 @Component({
@@ -16,31 +16,41 @@ export class WheelPicker {
   readonly items = input.required<number[]>();
   readonly value = input.required<number>();
   readonly valueChange = output<number>();
-  readonly name = input<string>('');
+  readonly name = input('');
 
   readonly wheelRef = viewChild<ElementRef<HTMLDivElement>>('wheel');
 
   readonly wheelHeight = WHEEL_HEIGHT;
   readonly itemHeight = WHEEL_ITEM_HEIGHT;
-  readonly centerOffset = WHEEL_OFFSET;
   private scrollTimer?: number;
+
+  readonly centerIndex = signal(0);
+
+  readonly displayItems = computed(() => {
+    const items = this.items();
+
+    return [...items, ...items, ...items];
+  })
 
   constructor() {
     afterNextRender(() => {
-      this.setWheelStartPosition();
+      this.scrollToValue(this.value());
     });
 
     effect(() => {
-      this.setWheelStartPosition();
+      this.scrollToValue(this.value());
     })
   }
 
-  private setWheelStartPosition() {
+  private scrollToValue(value: number) {
     const wheel = this.wheelRef()?.nativeElement;
     if (!wheel) return;
 
-    const index = this.items().indexOf(this.value());
-    wheel.scrollTop = index * this.itemHeight;
+    const baseIndex = this.items().indexOf(this.value());
+    if (baseIndex < 0) return;
+
+    const middleIndex = baseIndex + this.items().length;
+    wheel.scrollTop = middleIndex * this.itemHeight;
   }
 
   onScroll() {
@@ -54,10 +64,34 @@ export class WheelPicker {
     const wheel = this.wheelRef()?.nativeElement;
     if (!wheel) return;
 
-    const index = Math.round(wheel.scrollTop / this.itemHeight);
+    const itemCount = this.items().length;
+    let index = Math.round(wheel.scrollTop / this.itemHeight);
     wheel.scrollTop = index * this.itemHeight;
+    this.centerIndex.set(index);
 
-    const value = this.items()[index];
+    if (index < itemCount / 2) {
+      wheel.scrollTop += itemCount * this.itemHeight;
+      index += itemCount;
+    }
+    if (index > itemCount * 2.5) {
+      wheel.scrollTop = itemCount * this.itemHeight;
+      index -= itemCount;
+    }
+
+    const realIndex = index % itemCount;
+    const value = this.items()[realIndex];
     if (value !== undefined) this.valueChange.emit(value);
+  }
+
+  getItemClass(index: number): string {
+    const diff = index - this.centerIndex();
+
+    if (diff === 0) return 'center';
+    if (diff === -1) return 'near-top';
+    if (diff === 1) return 'near-bottom';
+    if (diff === -2) return 'far-top';
+    if (diff === 2) return 'far-bottom';
+
+    return 'hidden';
   }
 }
